f283044128e53a299146425221784dfa7429993b
sergey <serjjiniuss@gmail.com>
2025-02-19 22:55:28 +0400

    first commit

---
+++ app/src/main.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Editor from "./components/editor";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Editor />);
+
+// function getPageList() {
+//     $("h1").remove();
+//     $.get(
+//         "./api",
+//         (data) => {
+//             data.forEach((file) => {
+//                 $("body").append(`<h1>${file}</h1>`);
+//             });
+//         },
+//         "JSON"
+//     );
+// }
+
+// getPageList();
+
+// $("button").click(() => {
+//     $.post(
+//         "./api/createNewPage.php",
+//         {
+//             name: $("input").val(),
+//         },
+//         () => {
+//             getPageList();
+//         }
+//     ).fail(() => {
+//         alert("Страница уже существует");
+//     });
+// });
