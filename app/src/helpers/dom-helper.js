c8af43dbe7b9d48b702e83b129e7e4731b4b5c60
sergey <serjjiniuss@gmail.com>
2025-03-04 16:31:11 +0400

    upLoadImg+UI

--- app/src/helpers/dom-helper.js
+++ app/src/helpers/dom-helper.js
@@ -1,5 +1,4 @@
 export default class DOMHelper {
-
     static parseStrToDOM(str) {
         const parser = new DOMParser();
         return parser.parseFromString(str, "text/html");
@@ -10,20 +9,22 @@ export default class DOMHelper {
         let textNodes = [];
 
         function recursy(element) {
-            element.childNodes.forEach(node => {
-                
-                if(node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
+            element.childNodes.forEach((node) => {
+                if (
+                    node.nodeName === "#text" &&
+                    node.nodeValue.replace(/\s+/g, "").length > 0
+                ) {
                     textNodes.push(node);
                 } else {
                     recursy(node);
                 }
-            })
-        };
+            });
+        }
 
         recursy(body);
 
         textNodes.forEach((node, i) => {
-            const wrapper = dom.createElement('text-editor');
+            const wrapper = dom.createElement("text-editor");
             node.parentNode.replaceChild(wrapper, node);
             wrapper.appendChild(node);
             wrapper.setAttribute("nodeid", i);
@@ -38,8 +39,21 @@ export default class DOMHelper {
     }
 
     static unwrapTextNodes(dom) {
-        dom.body.querySelectorAll("text-editor").forEach(element => {
+        dom.body.querySelectorAll("text-editor").forEach((element) => {
             element.parentNode.replaceChild(element.firstChild, element);
+        });
+    }
+
+    static wrapImages(dom) {
+        dom.body.querySelectorAll("img").forEach((img, i) => {
+            img.setAttribute("editableimgid", i);
+        });
+        return dom;
+    }
+
+    static unwrapImages(dom) {
+        dom.body.querySelectorAll("[editableimgid]").forEach((img) => {
+            img.removeAttribute("editableimgid");
         });
     }
 }
